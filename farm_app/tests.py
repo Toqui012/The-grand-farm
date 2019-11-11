b2a19b744c465ba38e2e6c6a4d7d2b95e430eb30
Toqui012 <alvaroseba2000@gmail.com>
2019-11-11 01:36:15 -0300

    incorporacion de pruebas 2.0

--- farm_app/tests.py
+++ farm_app/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Payment_food
+import unittest
 
 # Create your tests here.
+
+def test_rut(a):
+        status = ''
+        list = Payment_food.objects.all()
+        for x in list:
+            if x.rut == a:
+                status = 'ENCONTRADO'
+                
+            else:
+                status = 'NO ENCONTRADO'
+        return status
+
+
+
+class TestProbar(unittest.TestCase):
+    def test_comparativo(self):
+        print('Ingrese el rut a buscar:')
+        x = input()
+        self.assertEqual(test_rut(x),'ENCONTRADO')
