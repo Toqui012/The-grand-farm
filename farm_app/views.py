ab2e2acb17afb7bcae7f8c6d82d2cab9ffc043eb
Toqui012 <alvaroseba2000@gmail.com>
2019-12-01 03:42:26 -0300

    entrega3 Final

--- farm_app/views.py
+++ farm_app/views.py
@@ -8,8 +8,6 @@ from django.template.context_processors import request
 from django.urls import reverse
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.core.mail import send_mail
-from django.conf import settings
 
 # Create your views here. 
 def payment_food_list(request):
